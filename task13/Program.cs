3c621d26e8dbf80c032b5024e6ea581b31cd1770
unknown <zh4rk1n@yandex.ru>
2023-06-14 11:55:52 +0300

    HW2_task10/13/15

---
+++ task13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int number = int.Parse (Console.ReadLine());
+if (number<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{ if ((number<1000)&&(number>99))
+{
+    number = number % 10;
+    Console.WriteLine(number);
+}
+else {
+    while (number>999){
+    number = number/10;
+}
+Console.WriteLine(number%10);
+}}
+
+    
---
+++ task15/Program.cs
@@ -0,0 +1,28 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number == 1) {
+        Console.WriteLine("Будний день");
+}
+if (number == 2) {
+        Console.WriteLine("Будний день");
+}
+if (number == 3) {
+        Console.WriteLine("Будний день");
+}
+if (number == 4) {
+        Console.WriteLine("Будний день");
+}
+if (number == 5) {
+        Console.WriteLine("Будний день");
+}
+if (number == 6) {
+        Console.WriteLine("Выходной день");
+}
+if (number == 7) {
+        Console.WriteLine("Выходной день");
+}
